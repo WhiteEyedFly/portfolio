cba1ac1e2b3a1783173df70b5f66d5e3990e0860
WhiteEyedFly <joshuawoodbridge2022@gmail.com>
2026-05-06 22:50:33 +0100

    js test 2 - server not running

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import(fs);
+import(path);
 
 export async function main(){
     const files = await fs.readdir("/projectpages", { withFileTypes: true });
