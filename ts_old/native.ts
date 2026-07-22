2ac66c399e1723296ae5633eca1ba425fc970b05
WhiteEyedFly <joshuawoodbridge2022@gmail.com>
2026-07-22 13:31:37 +0100

    removed old js and ts

--- ts_old/native.ts
+++
@@ -1,9 +0,0 @@
-import { renderLinks, loadJson } from "./shared.js";
-import type { Page } from "./types.js";
-
-async function nativeMain(): Promise<void> {
-    const pageList = await loadJson<Page[]>("../../data/otherPagesNative.json");
-    renderLinks(pageList);
-}
-
-nativeMain();
